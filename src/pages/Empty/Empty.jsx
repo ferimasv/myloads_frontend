94f7ba74bcd98023c03f53787dd70f50f52d72b6
ferimasv <90363353+ferimasv@users.noreply.github.com>
2023-01-05 18:14:11 +0500

    feat: added empty page

---
+++ src/pages/Empty/Empty.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Empty.module.css';
+
+const Empty = () => {
+    return (
+        <div className={classes.empty}>Чтобы посмотреть информацию, перейдите по вкладкам выше</div>
+    );
+};
+
+export default Empty;
