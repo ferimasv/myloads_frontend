1dbb12f00d0b30d047d62b6fe0704c04c557956b
ferimasv <90363353+ferimasv@users.noreply.github.com>
2023-01-05 18:18:01 +0500

    feat: init page employees

--- src/pages/Employees/Employees.jsx
+++ src/pages/Employees/Employees.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import classes from './Employees.module.css';
+import Search from "../../components/UI/Search/Search";
+import Button from "../../components/UI/Button/Button";
 
 const Employees = () => {
+    const employees = [];
+
     return (
         <div>
+            <div className={classes.row}>
+                <div className={classes.column}>
+                    <h2>Количество сотрудников - {employees.length}</h2>
+                </div>
+            </div>
 
+            <div className={classes.row}>
+                <div className={classes.column}>
+                    <Button>Добавить сотрудника</Button>
+                </div>
+
+                <div className={classes.column}>
+                    <Search/>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Outlet} from "react-router-dom";
 
 const Profile = () => {
     return (
         <div>
-
+            <Outlet/>
         </div>
     );
 };
